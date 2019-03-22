93f960c0907e52935ea9b80d41db2c5b6c0ddd23
slees <slees@hubspot.com>
2019-03-22 18:31:18 +0000

    Added some random text

--- project/js/main.js
+++ project/js/main.js
@@ -1,4 +1,3 @@
-
 function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
@@ -6,6 +5,10 @@ function myFunction() {
   $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+  $("#doge-image").append(
+    `<img class="img-circle" src="images/wowdoge.jpeg" />`
+  );
+  if (Math.floor(Math.random() * Math.floor(3)) === 2) {
+    $("#doge-image").append("<p>Cool</p>");
+  }
 }
-
